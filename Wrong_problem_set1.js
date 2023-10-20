6ec9dab0faf15f71dd2685122ea3b1289a0c2c48
AlanChen <419287484@qq.com>
2023-10-21 01:51:37 +0800

    prototype

--- Wrong_problem_set1.js
+++ Wrong_problem_set1.js
@@ -1,3 +1,13 @@
+/*
+ * @Author: 陈德立*******419287484@qq.com
+ * @Date: 2023-10-16 22:43:44
+ * @LastEditTime: 2023-10-21 01:30:57
+ * @LastEditors: 陈德立*******419287484@qq.com
+ * @Github: https://github.com/Alan1034
+ * @Description: 
+ * @FilePath: \前端面试错题集\Wrong_problem_set1.js
+ * 
+ */
 
 
 const problem1 = () => {
@@ -32,4 +42,17 @@ const problem3 = () => {
   // "{say:f}"
 }
 
+const problem4 = () => {
+  function Person(){}
+  Person.prototype.name='name'
+
+  var person=new Person()
+  person.name='name of this person'
+  console.log(person.name)
+  //name of this person
+
+  delete person.name
+  console.log(person.name)
+   //name
+}
 
